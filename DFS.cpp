ca7c2f4676889611f4f053760f90e9de76c016ac
mjngxwnj <mjngxujian289@gmail.com>
2024-05-21 22:03:20 +0700

    DFS

--- DFS.cpp
+++ DFS.cpp
@@ -1,55 +1,46 @@
 #include<iostream>
 #include<vector>
-#include<queue>
-#include<cstring>
 using namespace std;
-vector<long>adj[100001];
-bool check[100001];
-int cnt=1;
- void dfs(int u){
-	++cnt;
-	check[u]=true;
-	for(int i=0;i<adj[u].size();i++){
-		if(!check[adj[u][i]]){
-			dfs(adj[u][i]);
-		}
+
+bool visited[10001]={}; //Array marking vertices
+int vertices, edges;
+vector<int> adj[10001]; //vector save adjacency list
+//input adjacency list
+void input(){
+	cin >> vertices >> edges;
+	for(int i=0; i<edges; i++){
+		int x,y; cin>>x>>y;
+		adj[x].push_back(y);
+		adj[y].push_back(x);
 	}
-} 
-void bfs(int u){
-	queue<int> q;
-	q.push(u);
-	check[u]=true;
-	while(!q.empty()){
-		int v=q.front();
-		q.pop();
-		for(int i=0;i<adj[v].size();i++){
-			if(!check[adj[v][i]])
-			{
-					q.push(adj[v][i]);
-				check[adj[v][i]]=true;
-				++cnt;}
+}
+//DFS function
+void DFS(int u){
+	cout<<u<<" ";
+	visited[u] = true; //marking vertice u is visited
+	for(int i=0; i<adj[u].size();i++){
+		//check adjacent vertices
+		if(!visited[adj[u][i]]){
+			DFS(adj[u][i]);
 		}
 	}
 }
-int main()
-{
-	int n,k; cin>>n>>k;
-	for(int i=1;i<=n;i++){
-		int x; cin>>x;
-		for(int j=1;j<=x;j++){
-			int res; cin>>res;
-			adj[i].push_back(res);
-			//adj[res].push_back(i);
-		}
-	}	
-	memset(check,false,sizeof(check));
-	bfs(k);
-	cout<<cnt<<endl;
-	for(int i=1;i<=n;i++){
-		if(check[i]) {
-			cout<<i<<" ";
-		}
-	}
-/*	for(int i=0;i<adj[1].size();i++)
-	cout<<adj[1][i]<<" "; */
+int main(){
+	input();
+	/*1-----2-----3-----6
+	  |		|	  |
+	  9		|     8
+			4---5---7*/
+	/*9 8
+	  1 2
+	  1 9
+	  2 3
+	  2 4
+	  3 6
+	  3 8 
+	  4 5
+	  5 7
+	  ouput: 1 2 3 6 8 4 5 7 9 
+	*/
+	DFS(1);
 }
