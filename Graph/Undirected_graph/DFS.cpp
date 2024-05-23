4403e2ccd832378b5b22731e7ff8fe702954a2b2
mjngxwnj <mjngxujian289@gmail.com>
2024-05-23 19:42:12 +0700

    upload

---
+++ Graph/Undirected_graph/DFS.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+bool visited[10001]={}; //Array marking vertices
+int vertices, edges;
+vector<int> adj[10001]; //vector save adjacency list
+//input adjacency list
+void input(){
+	cin >> vertices >> edges;
+	for(int i=0; i<edges; i++){
+		int x,y; cin>>x>>y;
+		adj[x].push_back(y);
+		adj[y].push_back(x);
+	}
+}
+//DFS function
+void DFS(int u){
+	cout<<u<<" ";
+	visited[u] = true; //marking vertice u is visited
+	for(int i=0; i<adj[u].size();i++){
+		//check adjacent vertices
+		if(!visited[adj[u][i]]){
+			DFS(adj[u][i]);
+		}
+	}
+}
+int main(){
+	input();
+	/*1-----2-----3-----6
+	  |	      |	    |
+	  9	      |     8
+		      4---5---7*/
+	/*9 8
+	  1 2
+	  1 9
+	  2 3
+	  2 4
+	  3 6
+	  3 8 
+	  4 5
+	  5 7
+	  ouput: 1 2 3 6 8 4 5 7 9 
+	*/
+	DFS(1);
+}
---
+++ Graph/Undirected_graph/Detect_Cycle.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int vertices, edges; 
+vector<int> adj[10001]; //vector save adjacency list
+bool visited[10001]={}; //array marking vertices
+int parent[10001]={}; //array marking parent 
+//input adjacency list
+void input(){
+	cin >> vertices >> edges;
+	for(int i=0; i<edges; i++){
+		int x,y; cin>>x>>y;
+		adj[x].push_back(y);
+		adj[y].push_back(x);
+	}
+}
+bool isCycle(int u){
+    visited[u] = true;
+    for(int i=0; i<adj[u].size();i++){
+        if(!visited[adj[u][i]]){
+            parent[adj[u][i]] = u;  
+            if(isCycle(adj[u][i])) return true;
+        }
+        else if(adj[u][i]!=parent[u]) return true;
+    }
+    return false;
+}
+int main(){
+    input();
+    /*1-----2-----3-----4-----7 
+                 / \
+                8   5-----6*/
+    /*8 7
+      1 2 
+      2 3
+      3 4
+      3 5 
+      3 8
+      4 7
+      5 6 
+      output: Graph doesn't contain cycle
+    */
+   /*1-----2-----3-----4-----7
+            \          |
+             6-------- 5*/
+    /*7 7
+      1 2 
+      2 3 
+      2 6 
+      3 4 
+      4 7 
+      4 5 
+      5 6 
+      output: Graph contains cycle
+    */
+   bool contain_cycle = 0;
+   for(int i=1; i<=vertices; i++){
+    if(!visited[i]&&isCycle(i)){
+      contain_cycle = 1;
+      break;
+    }
+   }
+    contain_cycle ? cout<<"Graph contains cycle\n" : cout<<"Graph doesn't contain cycle\n";
+}
